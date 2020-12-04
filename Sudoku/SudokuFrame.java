cadd9b0987492e3165d7f604a0f547c9d1b673aa
moonyi1 <70625492+moonyi1@users.noreply.github.com>
2020-12-04 22:45:14 +0900

    Update SudokuFrame.java

--- Sudoku/SudokuFrame.java
+++ Sudoku/SudokuFrame.java
@@ -1,24 +1,16 @@
 package sudoku;
 
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 
-import slidepuzzle.PuzzleButton;
-
-
 public class SudokuFrame extends JFrame {
-	
 	private SudokuBoard board;
 	private InputNumber number;
 	private SudokuButton[][] sb;
 	private Numberbutton[] nb;
 	private int[][] game_board;
-	private int panel_width;
-	private int panel_height;
 	private int board_size;
-	private int size = 60;
-	private Container c;
+	private int button_size = 60;
 	
 	public SudokuFrame(int bs, SudokuBoard b, InputNumber n) {
 		board_size = bs;
@@ -26,30 +18,25 @@ public class SudokuFrame extends JFrame {
 		nb = new Numberbutton[bs];
 		board = b;
 		number = n;
-		panel_width = (board_size + 4) * size;
-		panel_height = (board_size + 2) * size;
 		game_board = b.contents();
-		
-		c = getContentPane();
+		Container c = getContentPane();
 		c.setLayout(null);
 		for(int i = 0; i < board_size; i++) {
 			nb[i] = new Numberbutton(number,this);
-			nb[i].setBounds(size*(board_size+2), size*(i+1), size, size);
+			nb[i].setBounds(button_size*(board_size+2), button_size*(i+1), button_size, button_size);
 			c.add(nb[i]);
 			nb[i].setText(""+(i+1));
 		}
-		
 		for (int x = 0; x < board_size; x++) 
 			for (int y = 0; y < board_size; y++) {
-				sb[x][y] = new SudokuButton(x,y, board, this);
-				sb[x][y].setBounds((x+1)*size, (y+1)*size, size, size);
+				sb[x][y] = new SudokuButton(x,y, board, this, number);
+				sb[x][y].setBounds((x+1)*button_size, (y+1)*button_size, button_size, button_size);
 				sb[x][y].setBackground(Color.white);
 				c.add(sb[x][y]);
 			}
-		
 		update();
 		setTitle("Sudoku Game");
-		setSize(panel_width, panel_height);
+		setSize((board_size + 4) * button_size, (board_size + 2) * button_size);
 		setVisible(true);
 	}
 	
@@ -62,27 +49,9 @@ public class SudokuFrame extends JFrame {
 				else
 					sb[x][y].setText("");
 			}
-		
 		for (int i = 0; i < board_size; i++) {
 			if (i+1 == number.valueOf()) nb[i].setBackground(Color.yellow);
 			else nb[i].setBackground(Color.white);
 		}
-		
-		System.out.println("update check");
 	}
-	
-	/** return_input - 현재 선택한 숫자
-	 * @return 현재 선택한 숫자의 값
-	 */
-	public int return_input() { return number.valueOf(); }
-	
-	/** number_update - 새로 선택한 숫자 업데이트
-	 * @param num - 새로 선택한 숫자
-	 */
-	public void number_update(int num) { 
-		number.select(num); 
-		System.out.println(num);
-	}
-	
-
 }
