70fab0d2f19e4c76ff9afef73d34676071a7fe35
moonyi1 <70625492+moonyi1@users.noreply.github.com>
2020-12-04 22:43:56 +0900

    Update SudokuBoard.java

--- Sudoku/SudokuBoard.java
+++ Sudoku/SudokuBoard.java
@@ -105,9 +105,8 @@ public class SudokuBoard {
 			JOptionPane.showMessageDialog(null, "틀렸습니다. 다시 시도해보세요.");
 		}
 	}
-	 
-	public int returnEmpty() { return empty; }
-	public String returnDif() { 
+	
+	public String returnDif() {
 		if (difficulty == 1)
 			return "쉬움";
 		else if (difficulty == 2)
@@ -115,6 +114,8 @@ public class SudokuBoard {
 		else
 			return "어려움";
 	}
-	public int[][] returnAnswer() { return answer; }
+	
+	public int returnEmpty() { return empty; }
+
 	public int[][] contents() { return game_board; }
 }
