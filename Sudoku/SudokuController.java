a83f0c073defc9c272aebc57b64bd640f88aa947
moonyi1 <70625492+moonyi1@users.noreply.github.com>
2020-12-04 22:44:51 +0900

    Update SudokuController.java

--- Sudoku/SudokuController.java
+++ Sudoku/SudokuController.java
@@ -6,7 +6,6 @@ public class SudokuController {
 	private SudokuFrame frame;
 	private SudokuBoard board;
 
-	
 	public SudokuController(SudokuBoard b, SudokuFrame f) {
 		frame = f;
 		board = b;
@@ -18,7 +17,7 @@ public class SudokuController {
 			frame.update();
 		}
 		long t2 = System.currentTimeMillis();
-		JOptionPane.showMessageDialog(null, "다음에 또 만나요! \n"+"난이도: "+board.returnDif()+"\n클리어 시간: "+(t2-t1)/1000.00+" sec");
+		JOptionPane.showMessageDialog(null, "다음에 또 만나요! \n" + "난이도: " + board.returnDif()
+									  + "\n클리어 시간: " + (t2-t1)/1000.00 + " sec");
 	}
-
 }
