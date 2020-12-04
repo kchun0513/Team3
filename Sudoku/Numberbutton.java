de71be26ed2079bc92d9b130c5c860c5ade29a0f
moonyi1 <70625492+moonyi1@users.noreply.github.com>
2020-12-04 18:59:38 +0900

    Update Numberbutton.java

--- Sudoku/Numberbutton.java
+++ Sudoku/Numberbutton.java
@@ -4,17 +4,12 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Numberbutton extends JButton implements ActionListener {
-	
 	private InputNumber number;
 	private SudokuFrame frame;
 	
 	public Numberbutton(InputNumber n, SudokuFrame f) {
 		number = n;
-<<<<<<< HEAD
 		frame = f;
-=======
-		view = v;
->>>>>>> 5ee8e68affbe991a3403d3fdd95be1edcb3362b0
 		addActionListener(this);
 	}
 	
@@ -22,6 +17,6 @@ public class Numberbutton extends JButton implements ActionListener {
 		String s = getText();
 		number.select(Integer.parseInt(s));
 		frame.number_update(number.valueOf());
-		System.out.println(""+number.valueOf());
+		System.out.println("" + number.valueOf());
 	}
 }
