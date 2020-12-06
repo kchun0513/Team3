669adb7a71dd44326e4903de65748f0e595a747a
moonyi1 <70625492+moonyi1@users.noreply.github.com>
2020-12-06 18:18:56 +0900

    Update SudokuBoard.java

--- Sudoku/SudokuBoard.java
+++ Sudoku/SudokuBoard.java
@@ -1,6 +1,7 @@
 package sudoku;
 
 import javax.swing.*;
+import java.util.*;
 
 public class SudokuBoard {
 	private int[][] answer;
@@ -9,8 +10,9 @@ public class SudokuBoard {
 	private int empty;
 	private int difficulty;
 	
-	public SudokuBoard(int d) {
+	public SudokuBoard(int d, int b) {
 		difficulty = d;
+		board_size = b;
 		if (difficulty == 1) empty = 6;
 		else if (difficulty == 2) empty = 8;
 		else if (difficulty == 3) empty = 10;
@@ -23,8 +25,30 @@ public class SudokuBoard {
 	//정답 배열 제작
 	private void rowMaker() {
 		//첫째 줄
+		List<Integer> a = new ArrayList<Integer>();
 		for (int i = 0; i != board_size; i++)
-			answer[0][i] = i + 1;
+			a.add(i + 1);
+		Collections.shuffle(a);
+		for (int i = 0; i != board_size; i++)
+			answer[0][i] = a.get(i);
+		if (board_size == 4) {
+			fillBoard4x4();
+			shuffleRibbons(0);
+		}
+		else if (board_size == 6) {
+			fillBoard6x6();
+			shuffleRibbons(2);
+		}
+		else if (board_size == 9) {
+			fillBoard9x9();
+			shuffleRibbons(0);
+		}
+		transpose();
+		shuffleRibbons(0);
+		transpose();
+	}
+	
+	private void fillBoard4x4() {
 		//둘째 줄
 		answer[1][0] = answer[0][2];
 		answer[1][1] = answer[0][3];
@@ -40,39 +64,162 @@ public class SudokuBoard {
 		answer[3][1] = answer[2][3];
 		answer[3][2] = answer[2][0];
 		answer[3][3] = answer[2][1];
-		shuffle();
 	}
 	
-	private void shuffle() {
-		int i;
-		i = (int)(Math.random() * 2);
-		if (i == 1) {
-			int[] r = answer[0];
-			answer[0] = answer[1];
-			answer[1] = r;
+	private void fillBoard6x6() {
+		int[] a1 = {4,5,6,1,2,3};
+		int j = 0;
+		for(int i : a1) {
+			answer[1][j] = answer[0][i-1];
+			j++;
 		}
-		i = (int)(Math.random() * 2);
-		if (i == 1) {
-			int[] r = answer[2];
-			answer[2] = answer[3];
-			answer[3] = r;
+		int[] a2 = {3,1,2,6,4,5};
+		j = 0;
+		for(int i : a2) {
+			answer[2][j] = answer[0][i-1];
+			j++;
 		}
-		i = (int)(Math.random() * 2);
-		if (i == 1) {
-			for (int j = 0; j != board_size; j++) {
-				int k = answer[j][0];
-				answer[j][0] = answer[j][1];
-				answer[j][1] = k;
+		int[] a3 = {6,4,5,3,1,2};
+		j = 0;
+		for(int i : a3) {
+			answer[3][j] = answer[0][i-1];
+			j++;
+		}
+		int[] a4 = {2,3,1,5,6,4};
+		j = 0;
+		for(int i : a4) {
+			answer[4][j] = answer[0][i-1];
+			j++;
+		}
+		int[] a5 = {5,6,4,2,3,1};
+		j = 0;
+		for(int i : a5) {
+			answer[5][j] = answer[0][i-1];
+			j++;
+		}
+	}
+	
+	private void fillBoard9x9() {
+		int[] a1 = {4,5,6,7,8,9,1,2,3};
+		int j = 0;
+		for(int i : a1) {
+			answer[1][j] = answer[0][i-1];
+			j++;
+		}
+		int[] a2 = {7,8,9,1,2,3,4,5,6};
+		j = 0;
+		for(int i : a2) {
+			answer[2][j] = answer[0][i-1];
+			j++;
+		}
+		int[] a3 = {2,3,1,5,6,4,8,9,7};
+		j = 0;
+		for(int i : a3) {
+			answer[3][j] = answer[0][i-1];
+			j++;
+		}
+		int[] a4 = {5,6,4,8,9,7,2,3,1};
+		j = 0;
+		for(int i : a4) {
+			answer[4][j] = answer[0][i-1];
+			j++;
+		}
+		int[] a5 = {8,9,7,2,3,1,5,6,4};
+		j = 0;
+		for(int i : a5) {
+			answer[5][j] = answer[0][i-1];
+			j++;
+		}
+		int[] a6 = {3,1,2,6,4,5,9,7,8};
+		j = 0;
+		for(int i : a6) {
+			answer[6][j] = answer[0][i-1];
+			j++;
+		}
+		int[] a7 = {6,4,5,9,7,8,3,1,2};
+		j = 0;
+		for(int i : a7) {
+			answer[7][j] = answer[0][i-1];
+			j++;
+		}
+		int[] a8 = {9,7,8,3,1,2,6,4,5};
+		j = 0;
+		for(int i : a8) {
+			answer[8][j] = answer[0][i-1];
+			j++;
+		}
+	}
+	
+	private void shuffleRibbons(int a) {
+		List<int[]> a1 = new ArrayList<int[]>();
+		List<int[]> a2 = new ArrayList<int[]>();
+		List<int[]> a3 = new ArrayList<int[]>();
+		int[][] result = new int[board_size][board_size];
+		if (board_size == 4) {
+			a1.add(answer[0]);
+			a1.add(answer[1]);
+			a2.add(answer[2]);
+			a2.add(answer[3]);
+			Collections.shuffle(a1);
+			Collections.shuffle(a2);
+			a1.addAll(a2);
+			for (int i = 0; i != 4; i++)
+				result[i] = a1.get(i);
+		}
+		if (board_size == 6) {
+			if (a == 2) {
+				a1.add(answer[0]);
+				a1.add(answer[1]);
+				a2.add(answer[2]);
+				a2.add(answer[3]);
+				a3.add(answer[4]);
+				a3.add(answer[5]);
+				Collections.shuffle(a1);
+				Collections.shuffle(a2);
+				Collections.shuffle(a3);
+				a1.addAll(a2);
+				a1.addAll(a3);
 			}
-		}
-		i = (int)(Math.random() * 2);
-		if (i == 1) {
-			for (int j = 0; j != board_size; j++) {
-				int k = answer[j][2];
-				answer[j][2] = answer[j][3];
-				answer[j][3] = k;
+			else {
+				for (int i = 0; i != 3; i++)
+					a1.add(answer[i]);
+				for (int i = 3; i != 6; i++)
+					a2.add(answer[i]);
+				Collections.shuffle(a1);
+				Collections.shuffle(a2);
+				a1.addAll(a2);
 			}
+			for (int i = 0; i != 6; i++)
+				result[i] = a1.get(i);
 		}
+		if (board_size == 9) {
+			for (int i = 0; i != 3; i++)
+				a1.add(answer[i]);
+			for (int i = 3; i != 6; i++)
+				a2.add(answer[i]);
+			for (int i = 6; i != 9; i++)
+				a3.add(answer[i]);
+			Collections.shuffle(a1);
+			Collections.shuffle(a2);
+			Collections.shuffle(a3);
+			a1.addAll(a2);
+			a1.addAll(a3);
+			for (int i = 0; i != 9; i++)
+				result[i] = a1.get(i);
+		}
+		for (int i = 0; i != board_size; i++)
+			for (int j = 0; j != board_size; j++)
+				answer[i][j] = result[i][j];
+	}
+	
+	private void transpose() {
+		int[][] result = new int[board_size][board_size];
+		for (int i = 0; i != board_size; i++)
+			for (int j = 0; j != board_size; j++)
+				result[i][j] = answer[j][i];
+		for (int i = 0; i != board_size; i++)
+			for (int j = 0; j != board_size; j++)
+				answer[i][j] = result[i][j];
 	}
 	
 	//게임용 배열 제작
@@ -83,8 +230,8 @@ public class SudokuBoard {
 		int i = 0;
 		int x, y;
 		while (i < empty) {
-			x = (int)(Math.random() * 4.0);
-			y = (int)(Math.random() * 4.0);
+			x = (int)(Math.random() * (double)board_size);
+			y = (int)(Math.random() * (double)board_size);
 			if (game_board[x][y] != 0) {
 				game_board[x][y] = 0;
 				i++;
