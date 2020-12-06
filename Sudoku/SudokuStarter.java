af2a0ba92ef931cd5c19a7731a789e0d04c9542c
moonyi1 <70625492+moonyi1@users.noreply.github.com>
2020-12-06 18:19:25 +0900

    Update SudokuStarter.java

--- Sudoku/SudokuStarter.java
+++ Sudoku/SudokuStarter.java
@@ -7,7 +7,7 @@ public class SudokuStarter {
 	public static void main(String[] args) {
 		int board_size = 4;
 		int d = Integer.parseInt(JOptionPane.showInputDialog("1. 쉬움, 2. 보통, 3. 어려움"));
-		SudokuBoard board = new SudokuBoard(d);
+		SudokuBoard board = new SudokuBoard(d, board_size);
 		//SudokuWriter writer = new SudokuWriter(board, board_size);
 		SudokuFrame frame = new SudokuFrame(board_size,board,new InputNumber());
 		SudokuController controller = new SudokuController(board, frame);
